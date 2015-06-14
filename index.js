5ab4195391347f9b6ac8e0623282c47e7460c03b
Xavier Garcia Buils <xgbuils@gmail.com>
2015-06-14 15:13:49 +0200

    pushState testing and more browsers testing

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ objectAssign(Router.prototype, {
         if(options.pushState) {
             var current = this.getFragment()
             if (current !== path) {
-                this.pushState('/' + path);
+                this.pushState('/' + path)
                 this.trigger(path)
             }
         } else {
--- test/router_test.js
+++ test/router_test.js
@@ -4,7 +4,7 @@ var url = require('url')
 var objectAssign = require('object-assign')
 
 describe('Router', function () {
-	describe('location "http://sample.com/" and pushState false', function () {
+    describe('location "http://sample.com/" and pushState false', function () {
         beforeEach(function () {
             this.window = WindowShim('http://sample.com/')
             this.router = new Router({
@@ -18,7 +18,6 @@ describe('Router', function () {
         })
         describe('.getFragment', function () {
             it('returns correct path', function () {
-                console.log(this.router.getFragment())
                 this.router.getFragment().should.be.equal('')
             })
         })
@@ -137,20 +136,79 @@ describe('Router', function () {
             })
         })
     })
-    
+    describe('pushState true', function () {
+        beforeEach(function () {
+            this.start = function (location) {
+                this.window = WindowShim(location)
+                this.router = new Router({
+                    mock: {
+                        window: this.window
+                    }
+                })
+                this.router.start({
+                    pushState: true,
+                })
+            }
+            
+        })
+        describe('.getFragment', function () {
+            it('returns correct path', function () {
+                this.start('http://sample.com/')
+                this.router.getFragment().should.be.equal('')
+            })
+        })
+        describe('.navigate', function () {
+            it('getFragment() returns "about" when navigate to "about"', function () {
+                this.start('http://sample.com/index.html')
+                this.router.navigate('about')
+                this.router.getFragment().should.be.equal('about')
+            })
+            it('location is "http://sample.com/about" when navigate to "/about"', function () {
+                this.start('http://sample.com/section/')
+                this.router.navigate('/about')
+                this.window.location.href.should.be.equal('http://sample.com/about')
+            })
+            it('getFragment() returns "about?foo=bar" when navigate to "about?foo=bar"', function () {
+                this.start('http://sample.com/section/index.php')
+                this.router.navigate('about?foo=bar')
+                this.router.getFragment().should.be.equal('about?foo=bar')
+            })
+            it('location is "http://sample.com/section/about?foo=bar" when navigate to "/section/about?foo=bar"', function () {
+                this.start('http://sample.com/about/')
+                this.router.navigate('/section/about/?foo=bar')
+                this.window.location.href.should.be.equal('http://sample.com/section/about/?foo=bar')
+            })
+            it('getFragment() returns "about/?foo=bar" when navigate to "about/?foo=bar"', function () {
+                this.start('http://sample.com')
+                this.router.navigate('about/me/')
+                this.router.getFragment().should.be.equal('about/me')
+            })
+            it('location is "http://sample.com/about/?foo=bar" when navigate to "about/?foo=bar"', function () {
+                this.start('http://sample.com')
+                this.router.navigate('/about/me')
+                this.window.location.href.should.be.equal('http://sample.com/about/me')
+            })
+        })
+    })
 });
 
+function parseUrl (uri) {
+    var location = url.parse(uri)
+    Object.keys(location).forEach(function (e) {
+        location[e] || (location[e] = '')
+    })
+    return location
+}
+
 function WindowShim (uri) {
-    var location = {
-        href: uri
+    var _window = {
+        location: parseUrl(uri)
     }
-    return {
-        location: location,
-        history: {
-            pushState: function (state, title, path) {
-                location.href = url.resolve(location.href, path)
-                location = url.parse(location.href)
-            }
+    _window.history = {
+        pushState: function (state, title, path) {
+            var uri = url.resolve(_window.location.href, path)
+            _window.location = parseUrl(uri)
         }
     }
+    return _window
 }
